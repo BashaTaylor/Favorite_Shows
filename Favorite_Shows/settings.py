7181d6625a2e57ed1cf847bb2890ceac00c83aee
BashaTaylor <bashataylor.22@gmail.com>
2024-08-29 19:43:42 -0400

    U

--- Favorite_Shows/settings.py
+++ Favorite_Shows/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd&!=2hobd*xe004i@3br6b$uatz8+@+g4c)w(ec86&=-t-4kvp'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
