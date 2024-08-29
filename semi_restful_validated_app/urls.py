09e6c63c349e76f9b73ff39172b641901e33d8c3
BashaTaylor <bashataylor.22@gmail.com>
2024-08-28 23:42:36 -0400

    clean up after messing up

--- semi_restful_validated_app/urls.py
+++ semi_restful_validated_app/urls.py
@@ -15,16 +15,3 @@ urlpatterns = [
     path('shows/<int:show_id>/update', views.update, name='update'),  # Update a specific show
     path('shows/<int:show_id>/delete', views.delete, name='delete'),  # Delete a specific show
 ]
-
-
-# urlpatterns = [
-#     path('', views.index), #GET request redirecting to shows route
-#     path('show', views.show), #GET request to display all objects' info starting at a different root
-#     path('show/new', views.new), #GET request to display form to create an object ( a new show)
-#     path('show/create', views.create), #POST request to create an object
-
-#     path('shows/<int:show_id>/show', views.show), #GET request to display a specific object's info
-#     path('shows/<int:show_id>/edit', views.edit), #GET request to display form to update a specific object
-#     path('shows/<int:show_id>/update', views.update), #POST request to update a specific object
-#     path('shows/<int:show_id>/delete', views.delete), #POST request to delete a specific object
-# ]
