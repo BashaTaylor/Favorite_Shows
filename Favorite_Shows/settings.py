a415fc39a812f1b071fbbf00bb29cd9166579772
BashaTaylor <bashataylor.22@gmail.com>
2024-08-29 20:41:24 -0400

    up

--- Favorite_Shows/settings.py
+++ Favorite_Shows/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Optional: if you have a specific directory for static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Favorite_Shows_app/static')]
 
 # For production, you should configure `STATIC_ROOT`
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
